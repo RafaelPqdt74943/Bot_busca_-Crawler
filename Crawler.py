9f67bcd66de483316fcf0c4767447ee67d8115d9
RAFAEL SANTOS <sgtrafaelsantos0801@gmail.com>
2024-11-20 18:17:23 -0300

    instalando e usando a biblioteca BS4, SOUP

--- Crawler.py
+++ Crawler.py
@@ -1,5 +1,8 @@
 import requests
 
+from bs4 import BeautifulSoup
+
+
 
 URL_automoveis= "https://django-anuncios.solyd.com.br/automoveis/"
 
@@ -7,11 +10,25 @@ def buscar (url):
     try:
         resposta=requests.get(url)
         if resposta.status_code == 200 : 
-            print(resposta.text)
+           return resposta.text
         else:
-            print("ero ao fazer requisição")
+            print("erro ao fazer requisição")
     except Exception as error:
         print("erro ao fazer requisição")
         print(error)
         
-buscar(URL_automoveis)
+
+def parsing(resposta_HTML) : 
+    try:
+        soup = BeautifulSoup(resposta_HTML, 'html.parser') 
+        return soup
+    except Exception as error : 
+        print("erro ao fazer o parsing HTML")
+        print(error)     
+        
+resposta=buscar(URL_automoveis)  
+        
+if resposta:
+    soup=parsing(resposta)
+    print(soup.title) # manipulando o HTML
+    #print(soup.prettify()) # manipulando o HTML
