6d8c02e4a5d533bed8cee51d18655dbff88b39e4
RAFAEL SANTOS <sgtrafaelsantos0801@gmail.com>
2024-11-23 22:38:14 -0300

    criando o regex e a função para buscar telefones

--- Crawler.py
+++ Crawler.py
@@ -1,3 +1,5 @@
+import re
+
 import requests
 
 from bs4 import BeautifulSoup
@@ -7,7 +9,7 @@ Dominio = "https://django-anuncios.solyd.com.br"
 
 URL_automoveis= "https://django-anuncios.solyd.com.br/automoveis/"
 
-def buscar (url):
+def requisicao (url):
     try:
         resposta=requests.get(url)
         if resposta.status_code == 200 : 
@@ -28,24 +30,50 @@ def parsing(resposta_HTML) :
         print(error)   
         
 def encontrar_link (soup):
-    cards_pai = soup.find("div", class_= "ui three doubling link cards")
-    cards = cards_pai.find_all("a")
+    try:
+        cards_pai = soup.find("div", class_= "ui three doubling link cards")
+        cards = cards_pai.find_all("a")
+    except:
+        print(" erro ao encontrar link")
+        return None
     
     links = []
     for card in cards : 
-        link =card["href"]
-        links.append(link)
-    
+        try:
+            link =card["href"]
+            links.append(link)
+        except:
+            pass
     return links
+
+def encontrar_telefone (soup):
+    try:
+        descricao = soup.find_all("div", class_ ="sixteen wide column")[2].p.get_text().strip()
+    except:
+        print(" erro ao encontrar descrição")
+        return None
+    
+    regex = re.findall(r"\(?0?([1-9]{2})[\-\.\)]{0,2}(9[ \-\.]?\d{4})[ \-\.]?(\d{4})", descricao)
+    if regex:
+        return regex
+    
+    
+    
+
+
         
-resposta=buscar(URL_automoveis)  
+resposta_busca=requisicao(URL_automoveis)  
         
-if resposta:
-    soup=parsing(resposta)
-    if soup:
-        links = encontrar_link(soup)
-        print(links)
-            
+if resposta_busca:
+    soup_busca=parsing(resposta_busca)
+    if soup_busca:
+        links = encontrar_link(soup_busca)
+        for link in links :
+            resposta_anuncio=requisicao(Dominio + links[0])  
+            if resposta_anuncio : 
+                soup_anuncio = parsing(resposta_anuncio)       
+                if soup_anuncio : 
+                    print(encontrar_telefone(soup_anuncio))
     
     
     #print(soup.title) # manipulando o HTML
